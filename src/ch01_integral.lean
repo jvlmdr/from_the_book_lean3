37b396b37b4676e2612df3212e0187c50659b167
Jack Valmadre <jack.valmadre@gmail.com>
2023-03-04 15:23:34 +1100

    Make things cleaner

--- src/ch01_integral.lean
+++ src/ch01_integral.lean
@@ -24,38 +24,63 @@ end
 noncomputable def harmonic (n : ℕ) := (finset.Icc 1 n).sum (λ k, (↑k)⁻¹ : ℕ → ℝ)
 noncomputable def staircase (x : ℝ) := (⌊x⌋ : ℝ)⁻¹
 
--- To be used with antitone_on.interval_integrable.
-lemma antitone_on_staircase {m n : ℝ} (hm : 1 ≤ m) (hn : 1 ≤ n) : antitone_on staircase (set.uIcc m n) :=
+
+-- Show that staircase is antitone on [1, ∞).
+-- This will be used to show that it is integrable.
+
+lemma antitone_on_staircase : antitone_on staircase (set.Ici 1) :=
 begin
   rw antitone_on_iff_forall_lt,
+  simp,
   intro a, intro ha,
   intro b, intro hb,
-  have h1 := le_inf hm hn,
-  have ha : 1 ≤ a := le_trans h1 ha.left,
-  have hb : 1 ≤ b := le_trans h1 hb.left,
   intro hab,
   rw staircase, rw staircase,
   rw inv_le_inv,
-      simp,
-      rw int.le_floor,
-      apply le_of_lt,
-      exact lt_of_lt_of_le' hab (int.floor_le a),
+  { simp,
+    rw int.le_floor,
+    have h := int.floor_le a,
+    exact le_trans h (le_of_lt hab) },
+  { have h := int.lt_floor_add_one b,
     rw ← add_lt_add_iff_right (1 : ℝ), rw zero_add,
-    apply lt_of_le_of_lt hb,
-    apply int.lt_floor_add_one,
-  rw ← add_lt_add_iff_right (1 : ℝ), rw zero_add,
-  apply lt_of_le_of_lt ha,
-  apply int.lt_floor_add_one,
+    exact lt_of_le_of_lt hb h },
+  { have h := int.lt_floor_add_one a,
+    rw ← add_lt_add_iff_right (1 : ℝ), rw zero_add,
+    exact lt_of_le_of_lt ha h },
 end
+
+-- To be used with antitone_on.interval_integrable.
+lemma antitone_on_staircase_uIcc {a b : ℝ} (ha : 1 ≤ a) (hb : 1 ≤ b) : antitone_on staircase (set.uIcc a b) :=
+begin
+  apply antitone_on.mono antitone_on_staircase,
+  cases le_or_lt a b with hab hba,
+  { rw set.uIcc_of_le hab,
+    rw set.Icc_subset_Ici_iff hab,
+    exact ha },
+  { have hba := le_of_lt hba,
+    rw set.uIcc_comm,
+    rw set.uIcc_of_le hba,
+    rw set.Icc_subset_Ici_iff hba,
+    exact hb },
+end
+
+-- Short but used in two places.
+lemma interval_integrable_staircase {a b : ℝ} (ha : 1 ≤ a) (hb : 1 ≤ b)
+  : interval_integrable staircase measure_theory.measure_space.volume a b :=
+begin
+  apply antitone_on.interval_integrable,
+  apply antitone_on_staircase_uIcc ha hb,
+end
+
+-- -- Short but used in two places.
+-- lemma interval_integrable_staircase
+--   {a b : ℝ} (ha : 1 ≤ a) (hb : 1 ≤ b)
+--   : interval_integrable staircase measure_theory.measure_space.volume a b
+--   := antitone_on.interval_integrable (antitone_on_staircase ha hb)
 
 -- Some way to avoid writing `measure_theory.measure_space.volume` in `interval_integrable?
 -- variables {μ : measure_theory.measure ℝ} [measure_theory.is_locally_finite_measure μ]
 -- noncomputable def μ : measure_theory.measure ℝ := measure_theory.measure_space.volume
-
-lemma interval_integrable_staircase
-  {a b : ℝ} (ha : 1 ≤ a) (hb : 1 ≤ b)
-  : interval_integrable staircase measure_theory.measure_space.volume a b
-  := antitone_on.interval_integrable (antitone_on_staircase ha hb)
 
 -- lemma interval_integrable_staircase
 --   {μ : measure_theory.measure ℝ} [measure_theory.is_locally_finite_measure μ]
@@ -106,6 +131,12 @@ begin
   rw nat.lt_succ_iff,
 end
 
+lemma integral_step_eq {f : ℝ → ℝ} (n : ℕ) :
+  ∫ (x : ℝ) in step n..step (n + 1), f x = ∫ (t : ℝ) in ↑n..↑n + 1, f t :=
+begin
+  simp, rw step, rw step, simp,
+end
+
 lemma integral_staircase_eq_harmonic {n : ℕ} :
   ∫ (t : ℝ) in 1..(↑n + 1), staircase t = harmonic n :=
 begin
@@ -116,18 +147,12 @@ begin
   rw ← ha,  -- Replaces (1 : ℝ) not (1 : ℕ).
   have hmn : 1 ≤ n + 1 := by simp,
   rw ← interval_integral.sum_integral_adjacent_intervals_Ico hmn,
-    -- Need to prove sums are equal; ranges and values are equal.
+  { -- Prove sums are equal.
     -- The integral functions have (step ...) inside.
-    -- TODO: Avoid this line?
-  { have h : (λ (k : ℕ), ∫ (x : ℝ) in step k..step (k + 1), staircase x) = (λ (n : ℕ), ∫ (t : ℝ) in ↑n..↑n + 1, staircase t) := _,
-    { rw h,
-      rw (function.funext_iff.mpr integral_staircase_part_eq),
-      rw harmonic,
-      rw finset_Ico_succ },
-    { rw function.funext_iff,
-      intro k,
-      simp,
-      rw step, rw step, simp } },
+    rw function.funext_iff.mpr integral_step_eq,
+    rw function.funext_iff.mpr integral_staircase_part_eq,
+    rw harmonic,
+    rw finset_Ico_succ },
   { -- Prove each interval is integrable.
     intro k,
     intro hk,
