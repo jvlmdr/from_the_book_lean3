a3d5ba4b4a2323b720c60f89bef732f85ce3abbd
Jack Valmadre <jack.valmadre@gmail.com>
2023-04-04 12:53:10 +0930

    Finish telescoping prod

--- src/ch01_integral/main.lean
+++ src/ch01_integral/main.lean
@@ -9,6 +9,7 @@ import data.real.nnreal
 import data.real.ennreal
 import data.set.intervals.basic
 import data.nat.interval
+import data.subtype
 import order.filter.basic
 import order.filter.at_top_bot
 import order.monotone.basic
@@ -17,6 +18,7 @@ import topology.algebra.infinite_sum
 import algebra.group.basic
 import algebra.order.positive.field
 import algebra.field.defs
+import algebra.hom.group
 
 open real filter
 open_locale big_operators
@@ -44,32 +46,57 @@ begin
   sorry,
 end
 
--- lemma zero_lt_coe_pnat (x : ℕ+) : (↑x : nnreal) > 0 :=
--- begin
---   norm_cast,
---   exact x.prop,
--- end
-
--- def inv_coe_pos (x : ℕ+) : {y : ℝ // y > 0} := ⟨↑x, zero_lt_coe_pnat x⟩
-
--- noncomputable instance linear_ordered_comm_group_pos : linear_ordered_comm_group {x : ℝ // 0 < x} :=
+-- noncomputable instance linear_ordered_comm_group_positive : linear_ordered_comm_group {x : ℝ // 0 < x} :=
 --   positive.subtype.linear_ordered_comm_group
+-- noncomputable instance linear_ordered_cancel_comm_monoid_positive : linear_ordered_cancel_comm_monoid {x : ℝ // 0 < x} :=
+--   positive.subtype.linear_ordered_cancel_comm_monoid
+-- instance ordered_comm_monoid_positive : ordered_comm_monoid {x : ℝ // 0 < x} :=
+--   positive.subtype.ordered_comm_monoid
 
-noncomputable instance real_semifield : semifield ℝ := field.to_semifield
+instance comm_monoid_positive : comm_monoid {x : ℝ // 0 < x} :=
+  ordered_comm_monoid.to_comm_monoid _
 
-noncomputable instance real_comm_group_with_zero : comm_group_with_zero ℝ := semifield.to_comm_group_with_zero ℝ
+-- nonneg.coe_div
+@[simp, norm_cast]
+lemma positive_coe_div {a b : {x : ℝ // 0 < x}} : (↑(a / b) : ℝ) = ↑a / ↑b := rfl
 
-instance comm_group_nonzero : comm_group {x : ℝ // x ≠ 0} := sorry
+-- def positive_coe_hom : {x : ℝ // 0 < x} →+* ℝ := ⟨coe, coe_one, coe_mul, coe_add⟩
 
-instance can_lift_nonzero : can_lift ℝ {x : ℝ // x ≠ 0} _ _ := subtype.can_lift _
+-- Modelled on: `nnrat.coe_hom`, `inv_monoid_hom`
+def positive_coe_hom : {x : ℝ // 0 < x} →* ℝ :=
+{ to_fun := (λ x, x.val),
+  map_one' := by simp,
+  map_mul' := by simp, }
+
+-- Modelled on: `nnrat.coe_prod`
+@[norm_cast]
+theorem positive_coe_prod {α : Type*} {s : finset α} {f : α → {x : ℝ // 0 < x}} :
+  (↑(s.prod f) : ℝ) = ∏ (a : α) in s, ↑(f a) :=
+begin
+  -- Want equivalent of `ring_hom.map_prod`...
+  have h := positive_coe_hom.map_prod f,
+  simp [positive_coe_hom] at h,
+  exact h _,
+end
+
+-- TODO: Should be possible to prove with non-zero rather than positive.
+-- However, I couldn't find the right lemmas to make this easy.
+-- We can show that ℝ is instance of comm_group_with_zero... where to then?
+
+-- noncomputable instance real_semifield : semifield ℝ := field.to_semifield
+-- noncomputable instance real_comm_group_with_zero : comm_group_with_zero ℝ := semifield.to_comm_group_with_zero ℝ
+-- lemma finset_prod_range_div_of_ne_zero (f : ℕ → ℝ) (n : ℕ) (h_nonzero : ∀ k, f k ≠ 0) : 
+--   (finset.range n).prod (λ i, f (i+1) / f i) = f n / f 0 :=
 
 -- `finset.prod_range_div` requires `comm_group` (must exclude zero).
-lemma finset_prod_range_div_of_ne_zero (f : ℕ → ℝ) (n : ℕ) (h_pos : ∀ k, f k ≠ 0) :
-  (finset.range n).prod (λ (i : ℕ), f (i + 1) / f i) = f n / f 0 :=
+lemma finset_prod_range_div_of_positive (f : ℕ → ℝ) (n : ℕ) (hf : ∀ k, 0 < f k) :
+  (finset.range n).prod (λ i, f (i+1) / f i) = f n / f 0 :=
 begin
-  have h : ∀ (k : ℕ), f k = ↑(⟨f k, h_pos k⟩ : {x : ℝ // x ≠ 0}) := by simp,
-  have h' : ∀ (j k : ℕ), f j / f k = ↑(⟨f j / f k, h_pos (f j / f k)⟩ : {x : ℝ // x ≠ 0}),  -- Can obtain as subgroup_with_zero?
-  sorry,
+  have h := finset.prod_range_div (λ i, (⟨f i, hf i⟩ : {x : ℝ // 0 < x})) n,
+  simp [-finset.prod_div_distrib] at h,
+  rw ← subtype.coe_inj at h,
+  simp [positive_coe_prod, -finset.prod_div_distrib] at h,
+  exact h,
 end
 
 lemma finset_Icc_eq_image_succ_range : ∀ {k : ℕ}, finset.Icc 1 k = finset.image nat.succ (finset.range k) :=
@@ -107,7 +134,7 @@ begin
   { sorry, },
   apply le_trans h, clear h,
   apply le_of_eq,
-  have h := finset_prod_range_div_of_ne_zero (λ x, (↑x + 1 : ℝ)) (primes_le n).card _,
+  have h := finset_prod_range_div_of_positive (λ x, (↑x + 1 : ℝ)) (primes_le n).card _,
   rotate, { norm_cast, simp, },
   simp [-finset.prod_div_distrib] at h,
   norm_cast at h,
